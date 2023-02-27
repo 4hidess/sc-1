fd6aa53e48edafdf6434f419828cd3dfe455002c
Kang Mardhex <74248551+kmardhex@users.noreply.github.com>
2023-02-27 14:57:57 +0700

    Update add-wstls.sh

--- add-wstls.sh
+++ add-wstls.sh
@@ -38,9 +38,9 @@ cat>/etc/v2ray/$user-tls.json<<EOF
       "id": "${uuid}",
       "aid": "0",
       "net": "ws",
-      "path": "/chat",
+      "path": "/mdxstore",
       "type": "none",
-      "host": "",
+      "host": "${domain}",
       "tls": "tls"
 }
 EOF
@@ -61,7 +61,7 @@ echo -e "id             : ${uuid}"
 echo -e "alterId        : 0"
 echo -e "Security       : auto"
 echo -e "network        : ws"
-echo -e "path           : /chat"
+echo -e "path           : /mdxstore"
 echo -e "================================="
 echo -e "link TLS       : ${vmesslink1}"
 echo -e "================================="
