401f6b6a576f080a24b0047d9de5e415c9a7f177
Kang Mardhex <74248551+kmardhex@users.noreply.github.com>
2023-04-30 19:58:12 +0700

    Update add-wstls.sh

--- add-wstls.sh
+++ add-wstls.sh
@@ -38,9 +38,9 @@ cat>/etc/v2ray/$user-tls.json<<EOF
       "id": "${uuid}",
       "aid": "0",
       "net": "ws",
-      "path": "/",
+      "path": "/alsanet",
       "type": "none",
-      "host": "www.opensignal.com",
+      "host": "${domain}",
       "tls": "tls"
 }
 EOF
@@ -61,7 +61,7 @@ echo -e "id             : ${uuid}"
 echo -e "alterId        : 0"
 echo -e "Security       : auto"
 echo -e "network        : ws"
-echo -e "path           : /"
+echo -e "path           : /alsanet"
 echo -e "================================="
 echo -e "link TLS       : ${vmesslink1}"
 echo -e "================================="
